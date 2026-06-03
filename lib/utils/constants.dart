3b94fbb9d50fd0fed8f4883ea54cd5a490809c48
g-ai-001 <g-ai-002@ganecheng.tech>
2026-06-03 16:54:14 +0800

    chore: bump version to 0.4.6

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@
 
 class AppConstants {
   static const String appName = 'B站视频下载';
-  static const String version = '0.4.5';
+  static const String version = '0.4.6';
 
   // Bilibili API
   static const String bilibiliBaseUrl = 'https://api.bilibili.com';
